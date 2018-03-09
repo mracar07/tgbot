e17e00cbf882c9a92bfb4609d797d11d36e2de03
mracar07 <32598079+mracar07@users.noreply.github.com>
2018-03-09 22:25:04 +0300

    change default welcome messages

--- tg_bot/modules/sql/welcome_sql.py
+++ tg_bot/modules/sql/welcome_sql.py
@@ -5,8 +5,8 @@ from sqlalchemy import Column, String, Boolean, UnicodeText, Integer
 
 from tg_bot.modules.sql import SESSION, BASE
 
-DEFAULT_WELCOME = "Hey {first}, how are you?"
-DEFAULT_GOODBYE = "Nice knowing ya!"
+DEFAULT_WELCOME = "Merhaba {first}, Nasılsın?"
+DEFAULT_GOODBYE = "Sen bilirsin!"
 
 
 @unique
