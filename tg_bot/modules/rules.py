8f18ee4bb7e8e9ed71814aba538ee22373685466
mracar07 <32598079+mracar07@users.noreply.github.com>
2018-03-09 17:11:28 +0300

    translate fix

--- tg_bot/modules/rules.py
+++ tg_bot/modules/rules.py
@@ -33,7 +33,7 @@ def send_rules(update, chat_id, from_pm=False):
             raise
 
     rules = sql.get_rules(chat_id)
-    text = "The rules for *{}* are:\n\n{}".format(escape_markdown(chat.title), rules)
+    text = "*{}* Grubundaki kurallar:\n\n{}".format(escape_markdown(chat.title), rules)
 
     if from_pm and rules:
         bot.send_message(user.id, text, parse_mode=ParseMode.MARKDOWN)
