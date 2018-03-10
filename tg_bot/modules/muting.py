a775e8a021ee08a30480c789b21999ac3e2fab0c
mracar07 <32598079+mracar07@users.noreply.github.com>
2018-03-10 13:17:50 +0300

    translate to tr

--- tg_bot/modules/muting.py
+++ tg_bot/modules/muting.py
@@ -23,22 +23,22 @@ def mute(bot: Bot, update: Update, args: List[str]) -> str:
 
     user_id = extract_user(message, args)
     if not user_id:
-        message.reply_text("You'll need to either give me a username to mute, or reply to someone to be muted.")
+        message.reply_text("Kişinin mesajlarını engellemek için bir kullanıcı adı vermeli veya sessize alınacak birine cevap vermelisiniz.")
         return ""
 
     if user_id == bot.id:
-        message.reply_text("I'm not muting myself!")
+        message.reply_text("Kendimi susturamam!")
         return ""
 
     member = chat.get_member(int(user_id))
 
     if member:
         if is_user_admin(chat, user_id, member=member):
-            message.reply_text("Afraid I can't stop an admin from talking!")
+            message.reply_text("Korkarım ki bir yöneticinin konuşmasını susturamam!")
 
         elif member.can_send_messages is None or member.can_send_messages:
             bot.restrict_chat_member(chat.id, user_id, can_send_messages=False)
-            message.reply_text("Muted!")
+            message.reply_text("Sessize alındı!")
             return "<b>{}:</b>" \
                    "\n#MUTE" \
                    "\n<b>Admin:</b> {}" \
@@ -47,9 +47,9 @@ def mute(bot: Bot, update: Update, args: List[str]) -> str:
                                               mention_html(member.user.id, member.user.first_name))
 
         else:
-            message.reply_text("This user is already muted!")
+            message.reply_text("Bu kullanıcı zaten sessize alındı!")
     else:
-        message.reply_text("This user isn't in the chat!")
+        message.reply_text("Bu kullanıcı sohbetde değil!")
 
     return ""
 
@@ -65,7 +65,7 @@ def unmute(bot: Bot, update: Update, args: List[str]) -> str:
 
     user_id = extract_user(message, args)
     if not user_id:
-        message.reply_text("You'll need to either give me a username to unmute, or reply to someone to be unmuted.")
+        message.reply_text("Sesi açması için bir kullanıcı adı vermeniz veya sesi kapalı birine yanıt vermeniz gerekir..")
         return ""
 
     member = chat.get_member(int(user_id))
@@ -73,14 +73,14 @@ def unmute(bot: Bot, update: Update, args: List[str]) -> str:
     if member.status != 'kicked' and member.status != 'left':
         if member.can_send_messages and member.can_send_media_messages \
                 and member.can_send_other_messages and member.can_add_web_page_previews:
-            message.reply_text("This user already has the right to speak.")
+            message.reply_text("Bu kullanıcı zaten konuşma hakkına sahip.")
         else:
             bot.restrict_chat_member(chat.id, int(user_id),
                                      can_send_messages=True,
                                      can_send_media_messages=True,
                                      can_send_other_messages=True,
                                      can_add_web_page_previews=True)
-            message.reply_text("Unmuted!")
+            message.reply_text("Sesi açıldı!")
             return "<b>{}:</b>" \
                    "\n#UNMUTE" \
                    "\n<b>Admin:</b> {}" \
@@ -88,19 +88,19 @@ def unmute(bot: Bot, update: Update, args: List[str]) -> str:
                                               mention_html(user.id, user.first_name),
                                               mention_html(member.user.id, member.user.first_name))
     else:
-        message.reply_text("This user isn't even in the chat, unmuting them won't make them talk more than they "
-                           "already do!")
+        message.reply_text("Bu kullanıcı sohbette bile değil, "
+                           "Sesini açmam için grupta bulunmalı!")
 
     return ""
 
 
 __help__ = """
-*Admin only:*
- - /mute <userhandle>: silences a user. Can also be used as a reply, muting the replied to user.
- - /unmute <userhandle>: unmutes a user. Can also be used as a reply, muting the replied to user.
+*Sadece yöneticiler:*
+ - /mute <userhandle>: bir kullanıcıyı susturur. Yanıt olarak da kullanılabilir, kullanıcıya verilen cevaplar gizlenir.
+ - /unmute <userhandle>: bir kullanıcının sesini açar. Yanıt olarak da kullanılabilir, kullanıcıya verilen cevaplar gizlenir.
 """
 
-__mod_name__ = "Muting"
+__mod_name__ = "Susturma"
 
 MUTE_HANDLER = CommandHandler("mute", mute, pass_args=True, filters=Filters.group)
 UNMUTE_HANDLER = CommandHandler("unmute", unmute, pass_args=True, filters=Filters.group)
