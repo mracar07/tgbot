4b4c6fc6269b182be21afce219dfbf9b03cd326c
mracar07 <32598079+mracar07@users.noreply.github.com>
2018-03-10 10:28:20 +0300

    fix

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -190,7 +190,7 @@ def invite(bot: Bot, update: Update):
 @run_async
 def adminlist(bot: Bot, update: Update):
     administrators = update.effective_chat.get_administrators()
-    text = "Admins in *{}*:".format(update.effective_chat.title or "this chat")
+    text = "*{}* Sohbetindeki Yöneticiler :".format(update.effective_chat.title or "this chat")
     for admin in administrators:
         user = admin.user
         name = "[{}](tg://user?id={})".format(user.first_name + (user.last_name or ""), user.id)
